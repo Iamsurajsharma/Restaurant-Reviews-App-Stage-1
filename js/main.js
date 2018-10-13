47d90aa0825b2fa9f9660908707ae11fcec68833
Suraj Sharma <mailforsurajsharma@gmail.com>
2018-10-13 12:58:31 +0530

    added role, tab and alt

--- js/main.js
+++ js/main.js
@@ -161,9 +161,14 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  if(restaurant.name !== 'Emily') {
+    image.alt = `An image of restaurant ${restaurant.name}`;
+} else {
+    image.alt = `An image of food item from ${restaurant.name} restaurant.`;
+}
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,6 +89,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
@@ -128,6 +129,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
   const title = document.createElement('h2');
   title.innerHTML = 'Reviews';
+  title.setAttribute("tabindex", 0);
   container.appendChild(title);
 
   if (!reviews) {
@@ -148,6 +150,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
+  li.setAttribute("tabindex", 0);
   const name = document.createElement('p');
   name.innerHTML = review.name;
   li.appendChild(name);
